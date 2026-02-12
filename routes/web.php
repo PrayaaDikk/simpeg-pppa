8b64d723d9ff23bb895ddbc3364405b365ac10d8
PrayaaDikk <fadilprayadika@gmail.com>
2026-02-12 10:21:37 +0800

    backup before installing filament

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 
-Route::prefix('admin')->group(function () {
-    Route::Get('/', [HomeController::class, 'index'])->name('dashboard');
-});
+Route::Get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
