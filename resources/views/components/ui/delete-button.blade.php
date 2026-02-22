677cfcb54a05a3454852255df7776eafcf467ba9
Fadil Prayadika <151795256+PrayaaDikk@users.noreply.github.com>
2026-02-23 05:47:09 +0800

    Fix button formatting in delete-button component

--- resources/views/components/ui/delete-button.blade.php
+++ resources/views/components/ui/delete-button.blade.php
@@ -61,7 +61,7 @@
                             </button>
                         </form>
 
-                        <button type="button" data-modal-hide="{{ $modalId }}" <<<<<<< HEAD
+                        <button type="button" data-modal-hide="{{ $modalId }}"
                             class="cursor-pointer text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 transition-colors">
                             {{ $cancelText }}
                         </button>
